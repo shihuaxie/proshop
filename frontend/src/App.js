ef2db58c29c7162ecaee86159fd3c197b250d5ae
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-08-03 16:43:16 +1000

    feat: add list products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import {Container} from "react-bootstrap";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
     return (
@@ -8,7 +9,7 @@ const App = () => {
             <Header/>
             <main className="py-3">
             <Container>
-                <h1>Welcome to ProShop</h1>
+                <HomePage/>
             </Container>
             </main>
             <Footer/>
