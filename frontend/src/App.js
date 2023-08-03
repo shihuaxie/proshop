8e1a207f9167dbebd01295d59ea4c0cdde779916
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-08-03 15:13:54 +1000

    feat: add on header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-function App() {
-  return (
+import Header from "./components/Header";
+import {Container} from "react-bootstrap";
+import Footer from "./components/Footer";
 
-  <h1>Hello react</h1>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <main className="py-3">
+            <Container>
+                <h1>Welcome to ProShop</h1>
+            </Container>
+            </main>
+            <Footer/>
+        </>
+    );
+};
 
 export default App;
