94547507a01a262918f4bff9a876c012a58cfd27
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-08-15 17:34:24 +1000

    feat: create product routs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); //connect to mongoDB
@@ -13,13 +13,6 @@ app.get('/', (req, res)=>{
     res.send('api is running on 5000')
 })
 
-app.get('/api/products', (req, res)=>{
-    res.json(products);
-})
-
-app.get('/api/products/:id', (req, res)=>{
-    const product =  products.find((p)=>p._id === req.params.id);
-    res.json(product);
-})
+app.use('/api/products', productRoutes);
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`))
