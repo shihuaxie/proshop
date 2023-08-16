2350528ac73204d41d928bb359ad5c1e2c946899
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-08-16 13:59:18 +1000

    feat: create product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,8 @@
 import express from "express";
-
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
+import {getProductById, getProducts} from "../controllers/productController.js";
 
-import Product from "../models/productModel.js";
-
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({}); //this will find all products
-    // throw new Error('Some error');
-    res.json(products);
-}));
-
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-        res.json(product);
-    }
-    res.status(404);
-    throw new Error('Resource not found');
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
