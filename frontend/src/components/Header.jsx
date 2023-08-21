426945b9988e428b8bf336d09d730fa6992249f2
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-08-21 12:43:51 +1000

    feat: item count in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,39 @@
-import {Navbar, Nav, Container} from "react-bootstrap";
+import {Badge, Navbar, Nav, Container} from "react-bootstrap";
 import {FaShoppingCart, FaUser} from "react-icons/fa";
 import {LinkContainer} from "react-router-bootstrap";
+import {useSelector} from "react-redux";
 import logo from "../assets/logo.png";
 
 const Header = () => {
+    const {cartItems} = useSelector((state) => state.cart);
+    console.log(cartItems);
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                   <LinkContainer to="/">
-                       <Navbar.Brand>
-                           <img src={logo} alt='ProShop'/>
-                           ProShop
-                       </Navbar.Brand>
-                   </LinkContainer>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt='ProShop'/>
+                            ProShop
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <LinkContainer to="/cart">
-                                <Nav.Link><FaShoppingCart/>Cart</Nav.Link>
+                                <Nav.Link>
+                                    <FaShoppingCart/>Cart
+                                    {
+                                        cartItems.length > 0 && (
+                                            <Badge pill bg='success' style={{
+                                                marginLeft: '5px'
+                                            }}>
+                                                {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                            </Badge>
+
+                                        )
+                                    }
+                                </Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="/login">
                                 <Nav.Link><FaUser/>Sign in</Nav.Link>
