818c170e9ad17ae7346b32933cea9c7d669d6ec5
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-09-04 12:02:20 +1000

    feat: create register page and functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
             <Route path="/product/:id" element={<ProductPage/>}/>
             <Route path="/cart" element={<CartPage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
         </Route>
     )
 );
--- frontend/src/slices/usersSliceApi.js
+++ frontend/src/slices/usersSliceApi.js
@@ -12,12 +12,18 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         }),
         logout: builder.mutation({
             query: () => ({
-                url:`${USERS_URL}/logout`,
+                url: `${USERS_URL}/logout`,
                 method: 'POST',
-            })
-        })
-
+            }),
+        }),
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}`,
+                method: 'POST',
+                body: data,
+            }),
+        }),
     }),
 });
 
-export const {useLoginMutation, useLogoutMutation} = usersApiSlice;
+export const {useLoginMutation, useLogoutMutation, useRegisterMutation} = usersApiSlice;
