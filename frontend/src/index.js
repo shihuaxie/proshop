578aafea954de10a5bc2e3a6c0cc01e31250c5c2
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-09-05 16:28:41 +1000

    feat: add payment method functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ShippingPage from "./pages/ShippingPage";
 import PrivateRoute from "./components/PrivateRoute";
+import PaymentPage from "./pages/PaymenyPage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
 
             <Route path='' element={<PrivateRoute/>}>
                 <Route path="/shipping" element={<ShippingPage/>}/>
+                <Route path="/payment" element={<PaymentPage/>}/>
             </Route>
         </Route>
     )
