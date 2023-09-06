84f70ccb73e439cb47b4900acf640f0eff4b2a09
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-09-06 15:52:03 +1000

    feat: add Order Routes and Order Controllers

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+import {
+    addOrderItems,
+    getAllOrders,
+    getMyOrders,
+    getOrderById,
+    updateOrderToDelivered,
+    updateOrderToPaid,
+} from "../controllers/orderController.js";
+
+import {protect, admin} from "../middleware/authMiddleware.js";
+
+
+router.route('/')
+    .post(protect, addOrderItems)
+    .get(protect, admin, getAllOrders);
+
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id').get(protect, admin, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect,admin,updateOrderToDelivered);
+
+
+export default router;
