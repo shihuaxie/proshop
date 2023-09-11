f3074830a6648a5a4b02307d60a0dbf3610ce503
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-09-11 14:47:49 +1000

    feat: add PayPalButtons to order page

--- frontend/src/pages/OrderPage.jsx
+++ frontend/src/pages/OrderPage.jsx
@@ -50,7 +50,44 @@ const OrderPage = () => {
                 }
             }
         }
-    }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal])
+    }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
+
+    function onApprove(data, actions) {
+        return actions.order.capture().then(async function (details) {
+            try {
+                await payOrder({orderId, details});
+                refetch();
+                toast.success('Payment Successful');
+            } catch (err) {
+                toast.error(err?.data?.message || err.message);
+            }
+        });
+    }
+
+    async function onApproveTest() {
+        await payOrder({orderId, details: {payer: {}}});
+        refetch();
+        toast.success('Payment Successful');
+    }
+
+    function onError(err) {
+        toast.error(err.message);
+    }
+
+    function createOrder(data, actions) {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: order.totalPrice,
+                    },
+                },
+            ],
+        }).then((orderId) => {
+            return orderId;
+        });
+    }
+
 
     return isLoading ? <Loader/> : error ? <Message variant="danger"/>
         : (
@@ -108,7 +145,7 @@ const OrderPage = () => {
                                                 <Link to={`/product/${item.product}`}>{item.name}</Link>
                                             </Col>
                                             <Col md={4}>
-                                                {item.qty} x {item.price} = ${item.qty * item.price}
+                                                {item.qty} x {item.price} = ${(item.qty * item.price).toFixed(2)}
                                             </Col>
                                         </Row>
                                     </ListGroup.Item>
@@ -141,7 +178,35 @@ const OrderPage = () => {
                                         <Col>${order.totalPrice}</Col>
                                     </Row>
                                 </ListGroup.Item>
+
                                 {/*{PAY ORDER PLACEHOLDER}*/}
+
+                                {!order.isPaid && (
+                                    <ListGroup.Item>
+                                        {loadingPay && <Loader/>}
+
+                                        {isPending ? (
+                                            <Loader/>
+                                        ) : (
+                                            <div>
+                                                {/*<Button onClick={onApproveTest}*/}
+                                                {/*        style={{marginBottom: '10px'}}*/}
+                                                {/*>*/}
+                                                {/*    Test Pay Order*/}
+                                                {/*</Button>*/}
+                                                <div>
+                                                    <PayPalButtons
+                                                        createOrder={createOrder}
+                                                        onApprove={onApprove}
+                                                        onError={onError}
+                                                    >
+                                                    </PayPalButtons>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </ListGroup.Item>
+                                )}
+
                                 {/*{MARK AS DELIVERED PLACEHOLDER}*/}
                             </ListGroup>
                         </Card>
